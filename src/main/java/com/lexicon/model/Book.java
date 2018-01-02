b062a381a96f33d02bcf261e21ce10d77252fe29
LenaHongXia <bjyuhongxia@gmail.com>
2018-01-02 11:06:08 +0100

    add Book entity

---
+++ src/main/java/com/lexicon/model/Book.java
@@ -0,0 +1,104 @@
+package com.lexicon.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	
+	private String ISBN;
+	private String genre;
+	private String title;
+	private String author;
+	private String shelf;
+	private String shelfRow;
+	private String shelfColumn;
+
+	public Book() {
+
+	}
+
+	public Book(String title, String ISBN, String genre, String author, String shelf, String shelfRow,
+			String shelfColumn) {
+		
+		this.title = title;
+		this.ISBN = ISBN;
+		this.genre = genre;
+		this.author = author;
+		this.shelf = shelf;
+		this.shelfRow = shelfRow;
+		this.shelfColumn = shelfColumn;
+
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getShelf() {
+		return shelf;
+	}
+
+	public void setShelf(String shelf) {
+		this.shelf = shelf;
+	}
+
+	public String getShelfRow() {
+		return shelfRow;
+	}
+
+	public void setShelfRow(String shelfRow) {
+		this.shelfRow = shelfRow;
+	}
+
+	public String getShelfColumn() {
+		return shelfColumn;
+	}
+
+	public void setShelfColumn(String shelfColumn) {
+		this.shelfColumn = shelfColumn;
+	}
+
+}
